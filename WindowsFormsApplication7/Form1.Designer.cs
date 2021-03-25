04f66c74cb5f4e17236835395c78809654b48193
Longhuyg <thailong2015py@gmail.com>
2021-03-25 14:13:57 +0700

    first commit

---
+++ WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtdangnhap = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.btndangnhap = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User";
+            // 
+            // txtdangnhap
+            // 
+            this.txtdangnhap.Location = new System.Drawing.Point(279, 105);
+            this.txtdangnhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtdangnhap.Name = "txtdangnhap";
+            this.txtdangnhap.Size = new System.Drawing.Size(132, 22);
+            this.txtdangnhap.TabIndex = 1;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(279, 149);
+            this.txtpass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(132, 22);
+            this.txtpass.TabIndex = 3;
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Location = new System.Drawing.Point(193, 158);
+            this.lblpass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(69, 17);
+            this.lblpass.TabIndex = 2;
+            this.lblpass.Text = "Password";
+            // 
+            // btndangnhap
+            // 
+            this.btndangnhap.Location = new System.Drawing.Point(197, 224);
+            this.btndangnhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btndangnhap.Name = "btndangnhap";
+            this.btndangnhap.Size = new System.Drawing.Size(100, 28);
+            this.btndangnhap.TabIndex = 4;
+            this.btndangnhap.Text = "Đăng nhập";
+            this.btndangnhap.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 223);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btndangnhap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 375);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btndangnhap);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.txtdangnhap);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtdangnhap;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.Button btndangnhap;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
